94ca037adf1da10b353ffebcd9f8b7e323afb563
pierre.hyvernat <pierre.hyvernat@univ-savoie.fr>
2013-12-19 17:15:28 +0100

    minor details, verbosity

--- main.ml
+++ main.ml
@@ -4,6 +4,8 @@ let _ =
   SCT.setOption "show_lambda" true; (* to show the x0 parameter, used for lambda counting in PML *)
   SCT.size_bound:=1;
   SCT.depth_bound:=2;
+  SCT.setOption "show_all_compositions" false;
+
   SCT.setOption "show_initial_call_graph" false ;
   SCT.setOption "show_collapsed_call_graph" false ;
   SCT.setOption "show_final_call_graph" false ;
@@ -12,6 +14,7 @@ let _ =
   SCT.setOption "show_decreasing_terms" false ;
   SCT.setOption "show_nondecreasing_coherents" false ;
   SCT.setOption "show_summary_TC" false;
+  SCT.setOption "use_approximates" true;
   ()
 
 let show_clauses = ref false
@@ -54,7 +57,7 @@ let main () =
           exit 2
       ) clauses;
 
-      if SCT.size_change_termination !graph
+      if SCT.size_change_termination_bounds !graph !SCT.depth_bound !SCT.size_bound
       then
         (print_string "OK, all functions terminate\n" ;
         exit 0)
@@ -63,18 +66,26 @@ let main () =
         exit 1)
 
 
+let set_verbosity v =
+  print_string "verbosity level of "; print_int v; print_newline();
+  if v>0 then (show_clauses := true; SCT.setOption "show_summary_TC" true);
+  if v>1 then SCT.setOption "show_initial_call_graph" true;
+  if v>2 then SCT.setOption "show_final_call_graph" true;
+  if v>3 then SCT.setOption "show_all_steps" true;
+  if v>4 then (SCT.setOption "show_coherents" true;
+               SCT.setOption "show_decreasing_terms" true;
+               SCT.setOption "show_nondecreasing_coherents" true);
+  if v>5 then SCT.setOption "show_all_compositions" true
+
 let _ =
   let args = [
-    ("-B", Arg.Int (fun b -> SCT.size_bound:=b), "\t\t\tset bound 'B'");
-    ("-D", Arg.Int (fun d -> SCT.depth_bound:=d), "\t\t\tset bound 'D'");
-    ("-v", Arg.Unit (fun _ -> SCT.setOption "show_summary_TC" true), "\t\t\tshow the incremental tests");
-    ("-show_definitions", Arg.Unit (fun _ -> show_clauses := true), "\tshow the definition (after parsing)");
-    ("-show_analysis", Arg.Unit (fun _ -> SCT.setOption "show_initial_call_graph" true), "\tshow the initial control-flow graph given by the static analysis");
-    ("-show_graph_of_paths", Arg.Unit (fun _ -> SCT.setOption "show_final_call_graph" true), "\tshow the graph of paths of the control-flow graph");
-    ("-show_decreasing", Arg.Unit (fun _ -> SCT.setOption "show_decreasing_terms" true), "\tshow the decreasing arguments of coherent loops");
-    ("-show_loops", Arg.Unit (fun _ -> SCT.setOption "show_nondecreasing_coherents" true), "\t\tshow the coherent loops which do not have decreasing arguments\n");
+    ("-B", Arg.Int (fun b -> SCT.size_bound:=b), "<n> set bound 'B' to n (n>0)");
+    ("-D", Arg.Int (fun d -> SCT.depth_bound:=d), "<n> set bound 'D' (n>=0)");
+    ("-v", Arg.Int (fun v -> set_verbosity v), "<n> shows various information (0<n<7)");
+    ("-no_subsumption", Arg.Unit (fun _ -> SCT.setOption "use_approximates" false), "doesn't use subsumption to simplify the graph of paths");
+    ("-show_definitions", Arg.Unit (fun _ -> show_clauses := true), "show the definition (after parsing)");
   ] in
-  let help = "usage: sct [-B <n>] [-D <n>] [...]\n" in
+  let help = "usage: sct [-B <n>] [-D <n>] [-v <n>] [...]\n" in
   let help = help^"reads definition from standard input\n" in
   let help = help^"available options:" in
   Arg.parse args print_endline help;
